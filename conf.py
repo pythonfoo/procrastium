bf2326cfdbbe87f882ec7745d8abb843ad60773d
bison-- <mike.bison42@googlemail.com>
2015-09-17 21:55:59 +0200

    first confable version

---
+++ conf.py
@@ -0,0 +1,17 @@
+__author__ = 'bison'
+
+# important for streaming images from localhost
+localIp = '1.2.3.4'
+
+pathWithImages = '/home/Photos'
+
+# in seconds.milsec
+showImageTime = 8.8
+
+# castmodes:
+# mirror = all casts the same image
+# random = all casts different images
+castmode = 'mirror'
+
+# chromecasts you dont want to stream on
+blacklist = []
---
+++ startCmd.py
@@ -0,0 +1,57 @@
+__author__ = 'bison'
+
+import os
+import pychromecast
+from pychromecast.controllers.youtube import YouTubeController
+from pychromecast.controllers.media import MediaController
+import time
+
+if os.path.isfile('conf_local.py'):
+    import conf_local as conf
+else:
+    import conf
+
+#import subprocess
+#proc = subprocess.Popen(['./startHttpServer.sh', conf.pathWithImages])
+
+#tmp = input('wait')
+#exit()
+
+print('start discovery')
+allCastNames = pychromecast.get_chromecasts_as_dict().keys()
+allCasts = {}
+print('end discovery')
+
+castIndex = {}
+for castName in allCastNames:
+    cast = pychromecast.get_chromecast(friendly_name=castName)
+    allCasts[castName] = cast
+    castIndex[castName] = len(castIndex)
+
+for castName, cast in allCasts.items():
+    #cast.quit_app()
+    pass
+
+print(castIndex)
+
+while True:
+    from os import listdir
+    from os.path import isfile, join
+    import random
+    onlyfiles = [ f for f in listdir(conf.pathWithImages) if isfile(join(conf.pathWithImages,f)) ]
+    imgFile = random.choice(onlyfiles)
+    for castName, cast in allCasts.items():
+        if not castName in conf.blacklist:
+            #cast.quit_app()
+
+            castMc = cast.media_controller
+
+            mc = MediaController()
+            cast.register_handler(mc)
+
+            if conf.castmode == 'random':
+                imgFile = onlyfiles[castIndex[castName]]
+
+            mc.play_media('http://'+ conf.localIp +':8000/'+ imgFile, 'Image/jpg')
+
+    time.sleep(conf.showImageTime)
